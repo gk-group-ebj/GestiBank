4a14393115ab8ca5a4207ad0a8ed247a9e5909f2
BSarpi <bricesarpi@hotmail.com>
2019-09-04 13:55:37 +0200

    new update: class user test de la méthode update user (sans BDD)

--- user.py
+++ user.py
@@ -1,11 +1,11 @@
 class User:
 
-    def __init__(self, id, name, surname, address, mail, password, phone=None, user_type=None):
+    def __init__(self, id, name, surname, address, email, password, phone=None, user_type=None):
         self.id = id
         self.name = name
         self.surname = surname
         self.address = address
-        self.mail = mail
+        self.email = email
         self.password = password
 
     # Méthode d'identification de l'utilisateur pour connexion et redirection
@@ -25,7 +25,7 @@ class User:
                 # exit
                 break
             else:
-                if input_mail is self.mail and input_password is self.password:
+                if input_email is self.email and input_password is self.password:
                     # Identification réussie
                     print("Identification réussie")
                     break  # Il faudra faire une redirection avec le user_type
@@ -41,29 +41,33 @@ class User:
         print("Vous êtes déconnecté")
 
     # Méthode de mise à jour de profil utilisateur
-    def update_profile(self, password, email, phone, address):
+    def update_profile(self):
 
         ask_password = input("Voulez-vous modifier votre mot de passe ? \n\noui ou non: ")
-        if ask_password is "oui":
-            password = input("Renseigneze \n\nnouveau mot de passe: ")
+        if ask_password == "oui":
+            new_password = input("Renseigneze \n\nnouveau mot de passe: ")
+            self.password = new_password
         else:
             pass
 
-        ask_mail = input("Voulez-vous modifier votre email ? \n\noui ou non: ")
-        if ask_mail is "oui":
-            mail = input("Renseignez \n\nnouvel email: ")
+        ask_email = input("Voulez-vous modifier votre email ? \n\noui ou non: ")
+        if ask_email == "oui":
+            new_email = input("Renseignez \n\nnouvel email: ")
+            self.email = new_email
         else:
             pass
 
         ask_phone = input("Voulez-vous modifier votre numéro de téléphone ? \n\noui ou non: ")
-        if ask_phone is "oui":
-            phone = input("Renseignez \n\nnouveau numéro: ")
+        if ask_phone == "oui":
+            new_phone = input("Renseignez \n\nnouveau numéro: ")
+            self.phone = new_phone
         else:
             pass
 
         ask_address = input("Voulez-vous modifier votre adresse ? \n\noui ou non: ")
-        if ask_address is "oui":
-            address = input("Renseignez \n\nnouvelle adress: ")
+        if ask_address == "oui":
+            new_address = input("Renseignez \n\nnouvelle adresse: ")
+            self.address = new_address
         else:
             pass
 
@@ -74,6 +78,8 @@ class User:
 
 # Partie test
 
-# u1 = User(5, "John", "Smith", "la matrice","a","b")
+u1 = User(5, "John", "Smith", "la matrice","a","b")
 
-# u1.login()
+#u1.login()
+u1.update_profile()
+print("new mdp : "+u1.password+"\nnew email : "+u1.email+"\nnouveau numéro :"+u1.phone+"\nnew adresse :"+u1.address)
