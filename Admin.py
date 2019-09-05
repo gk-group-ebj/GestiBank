973816946b73e4d0025a5816351b2da980061245
kardaoui <elmahdi.kardaoui@gmail.com>
2019-09-05 15:55:22 +0200

    creation de la class Admin avec methode creation et suppression de compte admin

---
+++ Admin.py
@@ -0,0 +1,74 @@
+from mysql.connector import Error, connection
+from user import User
+
+class Admin(User):
+    def __init__(self, id, name, surname, address, email, password, pro_id, phone, user_type):
+        self.pro_id = pro_id
+        User.__init__(self, id, name, surname, address, email, password , phone, user_type)
+
+    """méthode de création de compte Admin """
+
+    def creation_account_admin(self):
+        # id = input("entrer id : ")
+        self.name = input("entrer name : ")
+        self.surname = input("entrer surname  : ")
+        self.address = input("entrer address : ")
+        self.email = input("entrer email : ")
+        self.password = input("entrer password : ")
+        self.phone = input("entrer phone : ")
+        self.user_type = int(input("entrer user_type : "))
+        self.pro_id = input("entrer matricule : ")
+
+        conn = connection.MySQLConnection(host='localhost',
+                                          database='gesti_bank',
+                                          user='root')
+        mySql_insert_query = 'INSERT INTO admin (name, surname, address, email, password, phone, user_type, pro_id)' \
+                             '  VALUES (%s, %s, %s, %s, %s, %s, %s, %s)'
+
+        data = (self.name, self.surname, self.address, self.email, self.password, self.phone, self.user_type, self.pro_id)
+        cursor = conn.cursor()
+        result = cursor.execute(mySql_insert_query, data)
+        conn.commit()
+        conn.close()
+    def del_account_admin(selfself):
+        conn = connection.MySQLConnection(host='localhost',
+                                          database='gesti_bank',
+                                          user='root')
+        cursor = conn.cursor()
+        sql_Delete_query = """Delete from admin where id = %s"""
+        de_Id = 2
+        cursor.execute(sql_Delete_query, (de_Id,))
+        conn.commit()
+        conn.close()
+
+
+
+
+admin1 = Admin("2", "bob", "bach", "paris", "sonMail", "passe", "06458 ","185-45",2)
+#print("{0} admin {1} matricule {2}".format(admin1.name, admin1.id, admin1.pro_id))
+Admin.creation_account_admin(admin1)
+#Admin.del_account_admin(admin1)
+
+
+
+
+
+
+"""test de connexion a la base de données"""
+try:
+    connection = connection.MySQLConnection(host='localhost',
+                                            database='gesti_bank',
+                                            user='root')
+    if connection.is_connected():
+        cursor = connection.cursor()
+        cursor.execute("select database();")
+        record = cursor.fetchone()
+        print("Your connected to database: ", record)
+
+except Error as e:
+    print("Error while connecting to MySQL", e)
+finally:
+    if (connection.is_connected()):
+        connection.close()
+        print("MySQL connection is closed")
+
